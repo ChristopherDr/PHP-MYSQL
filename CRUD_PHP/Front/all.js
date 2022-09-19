30f70c1201602d5c1b0a752df41458318c58ad63
ChristopherDr <63821230+ChristopherDr@users.noreply.github.com>
2022-09-18 23:05:10 -0300

    Add files via upload

---
+++ CRUD_PHP/Front/all.js
@@ -0,0 +1,66 @@
+        
+var variavel
+
+//Uma variável que recebe uma função com parametro ID que joga o id selecionado para a URL informado no FETCH.
+//let excluir = (id) => fetch(`../back/delete.php?id=${id}`)
+
+
+async function excluir(id){
+   var teste = await fetch(`../back/delete.php?id=${id}`)
+    window.location.reload()
+
+}
+
+let editar = (id) => window.location.href = `cadastro.html?id=${id}`
+
+
+async function buscarDados(variavel){
+
+        var data = await fetch('../back/select.php');
+        var variavel = await data.json();
+        
+    var planilha = window.document.getElementById("tabela")
+   
+   var cabecalho = document.createElement("th")
+   cabecalho.innerText = " ID "
+   planilha.appendChild(cabecalho)
+   var cabecalho = document.createElement("th")
+   cabecalho.innerText = " NOME "
+   planilha.appendChild(cabecalho)
+   var cabecalho = document.createElement("th")
+   cabecalho.innerText = " CPF "
+   planilha.appendChild(cabecalho)
+
+    for ( var item of variavel){
+            var filho = document.createElement("tr")   
+            filho.innerHTML += 
+        `
+            <tr>
+
+                <td>${item.id}</td>
+                <td>${item.nome}</td>
+                <td>${item.cpf}</td>
+                <td>
+                    <button onclick="editar(${item.id})"> Editar </button>
+                    <button onclick="excluir(${item.id})"> Excluir </button>
+                </td>
+                
+            </tr>
+        `
+            planilha.appendChild(filho)
+        
+    }
+}
+
+async function listar(){
+    var resp = await  fetch(`../back/select.php`)
+    var variavel = await resp.json()
+    buscarDados(variavel)
+    
+  }
+
+  listar()
+
+  
+
+
